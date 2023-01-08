3c234a0c484419834a66acc83675ddcee13a72cd
amin <hm_bencheikh_lehocine@esi.dz>
2023-01-08 22:19:48 +0100

    adding script .sh to train adversarially BNN VGG16

---
+++ Bayesian_adversarial_training/train_BNN_adv.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+lr=0.01
+steps=10
+max_norm=0.01
+sigma_0=0.08
+init_s=0.08
+alpha=1.0
+data=cifar10
+root=./
+model=vgg
+model_out=./checkpoints/cifar10_vgg_adv_BNN.pth
+
+python ./train_BNN_adv.py \
+        --lr ${lr} \
+        --step ${steps} \
+        --max_norm ${max_norm} \
+        --sigma_0 ${sigma_0} \
+        --init_s ${init_s} \
+        --data cifar10 \
+        --model vgg \
+        --root ./ \
+        --model_out ${model_out} \
+        #--resume 
